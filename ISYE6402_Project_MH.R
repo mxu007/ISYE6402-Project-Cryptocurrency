3a01127e700882e80df92cd1a1509cbb2233bfca
Ming Han <mxu007@e.ntu.edu.sg>
2018-04-23 01:29:46 -0400

    export outputs for var and arimax

--- ISYE6402_Project_MH.R
+++ ISYE6402_Project_MH.R
@@ -1,10 +1,10 @@
-##ISYE6402 Project
+##ISYE6402 Project VAR and ARIMAX 
 library(data.table)
 library(zoo)
 library(vars)
 
 # Load data
-data <- read.csv("data/combined_crypto_daily_data new.csv",header=TRUE, stringsAsFactors = FALSE)
+data <- read.csv("data/combined_crypto_daily_data_new.csv",header=TRUE, stringsAsFactors = FALSE)
 data<- data[nrow(data):1,]
 rownames(data) = 1:nrow(data)
 
@@ -29,19 +29,8 @@ nasdaq <- data[, c(1,101:105)]
 usepu <- data[, c(1,106)]
 
 
-# ACF
-close <- cbind(bitcoin[,c(1,5)],ethereum[,5],ripple[,5], litecoin[,5], neo[,5], vix[,5], 
-               gold[,2], oil[,2], dji[,5], nasdaq[,5], usepu[,2]) 
-colnames(close) <- c("date", "bitcoin_close", "ethereum_close", "ripple_close", "litecoin_close", 
-                     "neo_close", "vix_close", "gold", "oil", "dji_close", "nasdaq_close", "usepu")
-
-# Create dataframe "close"
-#close <- cbind(bitcoin[,c(1,5)],ethereum[,5],ripple[,5], litecoin[,5], neo[,5]) 
-#colnames(close) <- c("date", "bitcoin_close", "ethereum_close", "ripple_close", "litecoin_close", "neo_close")
-close <- close[close$date >= start_date,]
-sapply(close,class)
-
-# Dynamically Split training and test set
+## Functions ##
+# Split training and test set
 train_test_split <- function(data, train_start_date, train_end_date, forecast_days, currency_index) {
   index_train_start = which(data$date == train_start_date)
   index_train_end = which(data$date == train_end_date)
@@ -50,7 +39,8 @@ train_test_split <- function(data, train_start_date, train_end_date, forecast_da
   return(list(currency_train, currency_test))
 }
 
-# ARIMA model fitting
+
+# ARIMA find optimal orders
 aic_optimal_order <- function(time_series) {
   n = length(time_series)
   p = c(0:5)
@@ -104,7 +94,7 @@ aic_optimal_order <- function(time_series) {
   return(invisible(NULL)) 
 }
 
-# forecast function
+# forecast function for var
 forecast_fun_var <- function(data, model, nvals, test) {
   l = length(data)
   
@@ -126,6 +116,18 @@ forecast_fun_var <- function(data, model, nvals, test) {
   return(list(forecast, ymax, ymin, npts, upper, lower))
 }
 
+# forecast function for arima
+forecast_fun_arima <- function(data, model, nvals, test) {
+  l = length(data)
+  forecast_values = predict(model, n.ahead=nvals, newxreg = test[,6:11])
+  upper_bound = forecast_values$pred + 1.96*abs(forecast_values$se)
+  lower_bound = forecast_values$pred - 1.96*abs(forecast_values$se)
+  npts = time(c(data, test))
+  ymax = max(max(upper_bound), max(data)) + 0.25
+  ymin = min(min(lower_bound), min(data)) - 0.25
+  return(list(forecast_values$pred, ymax, ymin, npts, upper_bound, lower_bound))
+}
+
 # performance measurement
 perf_measure <- function(forecast, test) {
   # MSPE 
@@ -139,7 +141,10 @@ perf_measure <- function(forecast, test) {
   return(c(mspe, mae, mape, pm))
 }
 
-# start date for all analyses, and moving training windows
+
+## Main ##
+
+# start date for all analyses, and moving training windows, window size = 30 days
 start_date = as.Date("2017-06-01")
 # start date vector for train
 train_start_date_windows = seq(start_date, max(data$date) - 31, "days")
@@ -148,8 +153,20 @@ train_end_date_windows = train_start_date_windows + 29
 # forecast window, 1-step prediction
 forecast_windows = c(1)
 
+# Subset input data to get close prices
+close <- cbind(bitcoin[,c(1,5)],ethereum[,5],ripple[,5], litecoin[,5], neo[,5], vix[,5], 
+               gold[,2], oil[,2], dji[,5], nasdaq[,5], usepu[,2]) 
+colnames(close) <- c("date", "bitcoin_close", "ethereum_close", "ripple_close", "litecoin_close", 
+                     "neo_close", "vix_close", "gold", "oil", "dji_close", "nasdaq_close", "usepu")
 
-### VAR Model ###
+# Create dataframe "close"
+#close <- cbind(bitcoin[,c(1,5)],ethereum[,5],ripple[,5], litecoin[,5], neo[,5]) 
+#colnames(close) <- c("date", "bitcoin_close", "ethereum_close", "ripple_close", "litecoin_close", "neo_close")
+close <- close[close$date >= start_date,]
+sapply(close,class)
+
+
+###---- VAR Model ----###
 df_perf = data.frame(matrix(nrow = length(train_start_date_windows)*
                               length(forecast_windows), ncol = 10, 0))
 colnames(df_perf) = c("currency", "train_start", "num_forecast_vals", "mspe", "mae", "mape", "pm", 
@@ -162,12 +179,9 @@ for(i in 1:length(train_start_date_windows)) {
     k = 2
     c(train,test) := train_test_split(close, train_start_date_windows[i], 
                                       train_end_date_windows[i], forecast_windows[j], k)
-    #print(diff(log(ts(train))))
     mul_fit <- VAR(diff(log(ts(train))), lag.max = 10, type="none")
     optimal_p <- mul_fit$p
-    #print((mul_fit$p))
     c(forecast, max, min, npts, ub, lb) := forecast_fun_var(train, mul_fit, forecast_windows[j], test)
-    
     c(mspe, mae, mape, pm) := perf_measure(forecast, test)
     
     df_perf[index, 1] = "bitcoin_close"
@@ -187,19 +201,19 @@ ymin <- min(min(df_perf[,9]),min(df_perf[,10]))
 ymax <- max(max(df_perf[,9]),max(df_perf[,10]))+20
 timevol <- time(df_perf[,10])
 plot(df_perf[,10], type="l", ylim = c(ymin,ymax), xlab="Time", ylab = "Price", main="Bitcoin Price in USD")
-#lines(timevol, df_perf[,9], lty=3, lwd=2, col="blue")
 lines(timevol, df_perf[,9], col="red")
 
+# Export result to csv
+write.csv(file = "data/var_analysis.csv", df_perf, row.names = F)
 
 
+###---- ARIMAX ----###
+df_perf_arimax = data.frame(matrix(nrow = length(train_start_date_windows)*
+                              length(forecast_windows), ncol = 14, 0))
 
-
-### ARIMAX ###
-
-df_perf = data.frame(matrix(nrow = length(train_start_date_windows)*
-                              length(forecast_windows), ncol = 10, 0))
-colnames(df_perf) = c("currency", "train_start", "num_forecast_vals", "mspe", "mae", "mape", "pm", 
-                      "optimal_p", "optimal_q", "optimal_d", "optimal_aic")
+colnames(df_perf_arimax) = c("currency", "train_start", "forecast_actual", "volatility", 
+                             "actual_val", "num_forecast_vals", "mspe", "mae", "mape", "pm", 
+                             "optimal_p", "optimal_q", "optimal_d", "optimal_aic")
 
 index = 1
 
@@ -207,22 +221,23 @@ for(i in 1:length(train_start_date_windows)) {
   for(j in 1:length(forecast_windows)) {
     k = 2
     c(train,test) := train_test_split(close, train_start_date_windows[i], 
-                                      train_end_date_windows[i], forecast_windows[j], k)
-    optimal_pdq = aic_optimal_order(train)
-    arimax_fit <- model_fin = arima(train, order = c(optimal_pdq[1]$p,optimal_pdq[3]$d,optimal_pdq[2]$q), 
-                                    method='ML',xreg = data.frame(train[,7:10]))
-    optimal_p <- mul_fit$p
-    #print((mul_fit$p))
-    c(forecast, max, min, npts, ub, lb) := forecast_fun_arima(train, mul_fit, forecast_windows[j], test)
+                                      train_end_date_windows[i], forecast_windows[j], c(k:12))
+    optimal_pdq = aic_optimal_order(log(train$bitcoin_close))
+    arimax_fit <- arima(log(train$bitcoin_close), order = c(optimal_pdq[1]$p,optimal_pdq[3]$d,optimal_pdq[2]$q), 
+                                    method='ML', xreg = data.frame(log(train[,6:11])))
+
+    c(forecast, max, min, npts, ub, lb) := forecast_fun_arima(log(train), arimax_fit, forecast_windows[j], log(test))
     
-    c(mspe, mae, mape, pm) := perf_measure(forecast, test)
+    c(mspe, mae, mape, pm) := perf_measure(forecast, log(test[,1]))
     
-    df_perf[index, 1] = "bitcoin_close"
-    df_perf[index, 2] = train_start_date_windows[i]
-    df_perf[index, 3] = forecast_windows[j]
-    df_perf[index, 4:7] = c(mspe, mae, mape, pm)
-    df_perf[index, 8] = optimal_p
-    df_perf[index, 9:10] = c(forecast,test)
+    df_perf_arimax[index, 1] = "bitcoin_close"
+    df_perf_arimax[index, 2] = train_end_date_windows[i] + 1
+    df_perf_arimax[index, 3] = exp(forecast[1])
+    df_perf_arimax[index, 4] = (exp(ub) - exp(lb))[1]
+    df_perf_arimax[index, 5] = (test[,1])
+    df_perf_arimax[index, 6] = forecast_windows[j]
+    df_perf_arimax[index, 7:10] = c(mspe, mae, mape, pm)
+    df_perf_arimax[index, 11:14] = optimal_pdq
     
     index = index + 1
     print(c(i, j))
@@ -230,51 +245,35 @@ for(i in 1:length(train_start_date_windows)) {
 }
 
 par(mfrow=c(1,1))
-ymin <- min(min(df_perf[,9]),min(df_perf[,10]))
-ymax <- max(max(df_perf[,9]),max(df_perf[,10]))+20
-timevol <- time(df_perf[,10])
-plot(df_perf[,10], type="l", ylim = c(ymin,ymax), xlab="Time", ylab = "Price", main="Bitcoin Price in USD")
-#lines(timevol, df_perf[,9], lty=3, lwd=2, col="blue")
-lines(timevol, df_perf[,9], col="red")
+ymin <- min(min(df_perf_arimax[,3]),min(df_perf_arimax[,5]))
+ymax <- max(max(df_perf_arimax[,3]),max(df_perf_arimax[,5]))+20
+timevol <- time(df_perf_arimax[,3])
+plot(df_perf_arimax[,5], type="l", ylim = c(ymin,ymax), xlab="Time", ylab = "Price", main="Bitcoin Price in USD")
+lines(timevol, df_perf_arimax[,3], col="red")
+
+# Export result to csv
+write.csv(file = "data/arimax_analysis.csv", df_perf_arimax, row.names = F)
 
 
-
-
-
-
-
-
-#----------------------------------------#
-close_train = close[1:(nrow(close)-7),]
-close_test = close[(nrow(close)-6):nrow(close),]
-close_train_ts <- ts(close_train[,-1])
+###---- VAR on the whole period starting from 06/01/17----###
+close_ts <- ts(close[,-1])
 
 # Plot time series
-plot(close_train_ts, plot.type="single", col = 1:ncol(close_train_ts))
-legend("topleft", colnames(close_train_ts), col=1:ncol(close_train), lty=1, cex=1)
+plot(close_ts, plot.type="single", col = 1:ncol(close_train_ts))
+legend("topleft", colnames(close_ts), col=1:ncol(close_ts), lty=1, cex=1)
 
 # ACF of log differenced time series
-acf(close_train_ts)
+acf(close_ts)
 
-close_ts_diff_log <- diff(log(close_train_ts))
+close_ts_diff_log <- diff(log(close_ts))
 plot(close_ts_diff_log, plot.type="single", col = 1:ncol(close_ts_diff_log))
-legend("topleft", colnames(close_ts_diff_log), col=1:ncol(close), lty=1, cex=0.7)
+legend("topleft", colnames(close_ts_diff_log), col=1:ncol(close_ts), lty=1, cex=0.7)
 
 acf(close_ts_diff_log)
 
-library(vars)
-mul_var_1 <- VAR(close_ts_diff_log, lag.max=20, type="none")
-mul_var_2 <- VAR(close_ts_diff_log, lag.max=20, type="const")
-mul_var_3 <- VAR(close_ts_diff_log, lag.max=20, type="trend")
-mul_var_4 <- VAR(close_ts_diff_log, lag.max=20, type="both")
-mul_var_1$p
-mul_var_2$p
-mul_var_3$p
-mul_var_4$p
-
-mul_fit <- VAR(close_ts_diff_log, lag.max=10, type="both")
-mul_fit$p
-restrict_mul_fit <- restrict(mul_fit)
+var_fit <- VAR(close_ts_diff_log, lag.max=20, type="both")
+var_fit$p
+restrict_var_fit <- restrict(var_fit)
 arch.test(mul_fit)
 normality.test(mul_fit)
 
